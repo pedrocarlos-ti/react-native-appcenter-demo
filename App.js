905dbe76d6cdeb68017a86e06acfc2fe1525cd73
Pedro santos <pedrocarlos.ti@gmail.com>
2020-03-09 16:39:29 -0300

    Add: Appcenter Wrapper

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import CodePush from 'react-native-code-push';
 
-export default function App() {
+function App() {
   return (
     <View>
       <Text>Hello</Text>
     </View>
   );
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(App);
